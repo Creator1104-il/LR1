5ada6b807f1de0b31b704231b98c5b8f88db0076
Ilia <ila04112007@gmail.com>
2025-05-02 18:46:59 +0300

    Initial commit

---
+++ app/src/main/java/com/example/LR1/MainActivity.java
@@ -0,0 +1,79 @@
+package com.example.LR1;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private int counter = 0;
+    private int counters = 0;
+//    private TextView counterText;
+//    private TextView countersText;
+    public void ClickOnButton(View view) {
+        counter++;
+        TextView textCounter = findViewById(R.id.textCounter);
+        String strcounter = Integer.toString(counter);
+        textCounter.setText(strcounter.toString());
+    }
+    public void ClickOnButton1(View view) {
+        counters++;
+        TextView textCounter = findViewById(R.id.textCounter1);
+        String strcounter = Integer.toString(counter);
+        textCounter.setText(strcounter.toString());
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        if (savedInstanceState != null &&
+                savedInstanceState.containsKey("counter")) {
+            counter = savedInstanceState.getInt("counter");
+        }
+        Log.d("Instance", "onRestoreInstanceState");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle autState){
+        super.onSaveInstanceState(autState);
+        autState.putInt("Нажалось", counter);
+        Log.d("Instens","onSave");
+        Toast.makeText(this, "onSave",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected  void onStart(){
+        super.onStart();
+        Log.d("Instens", "onStart");
+        Toast.makeText(this, "onStart", Toast.LENGTH_LONG).show();
+    }
+    @Override
+    protected  void onResume(){
+        super.onResume();
+        Log.d("Instens", "onResume");
+        Toast.makeText(this, "onResume", Toast.LENGTH_LONG).show();
+    }
+    @Override
+    protected  void onPause(){
+        super.onPause();
+        Log.d("Instens", "onPause");
+        Toast.makeText(this, "onPause", Toast.LENGTH_LONG).show();
+    }
+}
